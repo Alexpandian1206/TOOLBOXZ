2d95f1a82307e1de7967406fcc593ca0b6ed7b54
Alexpandian1206 <97244736+Alexpandian1206@users.noreply.github.com>
2025-08-14 20:56:09 +0530

    Update and rename tools-data.js to tools-list.js

--- tools-data.js
+++
@@ -1,6 +0,0 @@
-const toolsList = [
-  { catagory: "Text Tools", name: "Remove Line Breaks", img: "/wp-content/uploads/2025/07/Remove-Line-breaks.png", link: "/remove-line-breaks-tool/" },
-  { catagory: "Text Tools", name: "Word to PDF Converter", img: "/wp-content/uploads/2025/07/Word-to-PDF.png", link: "/word-to-pdf/" },
-  { catagory: "Text Tools", name: "PDF to Word Converter", img: "/wp-content/uploads/2025/07/PDF-to-Word.png", link: "/pdf-to-word/" }
-  // Add all other tools here
-];
