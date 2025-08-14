e70a1a27f799fd0a2332e60378dcd2897b859529
Alexpandian1206 <97244736+Alexpandian1206@users.noreply.github.com>
2025-08-14 20:03:55 +0530

    Create tools-data.js

---
+++ tools-data.js
@@ -0,0 +1,6 @@
+const toolsList = [
+  { name: "Remove Line Breaks", img: "/wp-content/uploads/2025/07/Remove-Line-breaks.png", link: "/remove-line-breaks-tool/" },
+  { name: "Word to PDF Converter", img: "/wp-content/uploads/2025/07/Word-to-PDF.png", link: "/word-to-pdf/" },
+  { name: "PDF to Word Converter", img: "/wp-content/uploads/2025/07/PDF-to-Word.png", link: "/pdf-to-word/" }
+  // Add all other tools here
+];
